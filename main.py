dd42b841ad2cb8bd047779931a6dff942945f36e
Orkhan Ahmadov <55695784+orikahmadov@users.noreply.github.com>
2020-12-16 00:25:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import requests, os, time, smtplib
 from bs4 import BeautifulSoup
 from email.message import EmailMessage
 
+"""THIS IS A PYTHON SCRIPT TO TRACK THE CHANGES OF PRICE OF THE  GIVEN AMAZON PRODUCT """
+
+
 def automate_price_checking():
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"}
